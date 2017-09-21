98e002819bedbd8254a3330d884237fd4741b803
csaiikumar <31348808+csaiikumar@users.noreply.github.com>
2017-09-21 13:13:51 -0400

    Update LaneModel.js

--- src/Components/LaneModel.js
+++ src/Components/LaneModel.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-changes made to the LaneModel.js
-
-changes made third time
-=======
 changes made to the ButtonModel.js
 changes made second time
->>>>>>> 67c770be4a48083e96b327b9a8ee5b81a950ea43
+
