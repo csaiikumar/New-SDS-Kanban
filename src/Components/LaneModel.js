20f431992ef6b186c08577d44888068e0af93c5f
saikumar <csaiikumar@gmail.com>
2017-09-21 13:43:17 -0400

    add

--- src/Components/LaneModel.js
+++ src/Components/LaneModel.js
@@ -1,4 +1,34 @@
-changes made to the ButtonModel.js
-changes made second time
-
-
+Saikumars-MacBook-Pro:New-SDS-Kanban saikumarchinthakuntla$ git status
+On branch master
+Your branch is up-to-date with 'origin/master'.
+Changes not staged for commit:
+(use "git add <file>..." to update what will be committed)
+(use "git checkout -- <file>..." to discard changes in working directory)	modified:   src/Components/LaneModel.js
+no changes added to commit (use "git add" and/or "git commit -a"
+Saikumars-MacBook-Pro:New-SDS-Kanban saikumarchinthakuntla$ git add .
+Saikumars-MacBook-Pro:New-SDS-Kanban saikumarchinthakuntla$ git status
+On branch master
+Your branch is up-to-date with 'origin/master'.
+Changes to be committed:
+(use "git reset HEAD <file>..." to unstage)
+modified:   src/Components/LaneModel.js
+Saikumars-MacBook-Pro:New-SDS-Kanban saikumarchinthakuntla$ git commit -m "update"
+[master e4accd6] update
+ 1 file changed, 1 insertion(+), 6 deletions(-)
+Saikumars-MacBook-Pro:New-SDS-Kanban saikumarchinthakuntla$ git status
+On branch master
+Your branch is ahead of 'origin/master' by 1 commit.
+  (use "git push" to publish your local commits)
+nothing to commit, working tree clean
+Saikumars-MacBook-Pro:New-SDS-Kanban saikumarchinthakuntla$ git pull
+Already up-to-date.
+Saikumars-MacBook-Pro:New-SDS-Kanban saikumarchinthakuntla$ git push
+Counting objects: 5, done.
+Delta compression using up to 4 threads.
+Compressing objects: 100% (3/3), done.
+Writing objects: 100% (5/5), 380 bytes | 0 bytes/s, done.
+Total 5 (delta 1), reused 0 (delta 0)
+remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+To https://github.com/csaiikumar/New-SDS-Kanban.git
+ce275c3..e4accd6  master -> master
+Saikumars-MacBook-Pro:New-SDS-Kanban saikumarchinthakuntla$ 
